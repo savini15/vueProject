ca1e51a52b3b58f31ebadade22193f7ef265ddb4
nyp <savini15@163.com>
2019-03-01 22:48:39 +0800

    'chucitijiao'

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+  <router-link to="/tab">tab</router-link>
+  <router-link to="/?id=10&name=aaa">首页</router-link>
+  <router-link to="/123/iik">首页</router-link>
+      <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
